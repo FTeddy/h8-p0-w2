fa1d23e1b6001abddae98f97d440d6a0db8b8e46
FTeddy <ainsophoure@gmail.com>
2017-12-18 13:19:52 +0700

    exercise 3 added

---
+++ exercise_3.js
@@ -0,0 +1,21 @@
+var tanggal= 10;
+var bulan= 1;
+var tahun= 1989;
+
+
+
+switch(bulan) {
+  case 1:   {console.log(String(tanggal)+ ' Januari ' + tahun); break;}
+  case 2:   {console.log(String(tanggal)+' Febuari ' + String(tahun)); break;}
+  case 3:   {console.log(String(tanggal)+' Maret ' + String(tahun)); break;}
+  case 4:   {console.log(String(tanggal)+' April ' + String(tahun)); break;}
+  case 5:   {console.log(String(tanggal)+' Mei ' + String(tahun)); break;}
+  case 6:   {console.log(String(tanggal)+' Juni ' + String(tahun)); break;}
+  case 7:   {console.log(String(tanggal)+' Juli ' + String(tahun)); break;}
+  case 8:   {console.log(String(tanggal)+' Agustus ' + String(tahun)); break;}
+  case 9:   {console.log(String(tanggal)+' September ' + String(tahun)); break;}
+  case 10:  {console.log(String(tanggal)+' Oktober ' + String(tahun)); break;}
+  case 11:  {console.log(String(tanggal)+' November ' + String(tahun)); break;}
+  case 12:  {console.log(String(tanggal)+' Desember ' + String(tahun)); break;}
+  default:  {console.log('Tolong lengkapi tanggal.')}
+}
