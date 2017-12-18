74b3ad73d20510e19251fe2864d3684a800c3905
FTeddy <ainsophoure@gmail.com>
2017-12-18 13:00:51 +0700

    added exercise 1 and 2, renamed files

---
+++ exercise_2.js
@@ -0,0 +1,39 @@
+var nama = '';
+var peran = '';
+
+//Ksatria, Tabib, Penyihir, Warrior of Light
+
+
+if(nama === '') {
+  console.log('Silahkan isi nama kamu!')
+}
+else {
+  if(peran == 'Warrior of Light'){
+    console.log('Welcome to Eorzea, ' + nama + ' the ' + peran + '!')
+
+  }
+  else {
+    if(peran == 'Tabib') {
+      console.log('Selamat datang di Dunia Proxytia, ' + nama +'.')
+      console.log('Halo '+ peran + ' ' + nama + ', kamu akan membantu temanmu yang terluka.')
+
+    }
+    else {
+      if(peran == 'Ksatria') {
+        console.log('Selamat datang di Dunia Proxytia, ' + nama +'.')
+        console.log('Halo '+ peran + ' ' + nama + ', kamu dapat menyerang dengan pedangmu!')
+
+      }
+      else {
+        if(peran == 'Penyihir') {
+          console.log('Selamat datang di Dunia Proxytia, ' + nama +'.')
+          console.log('Halo '+ peran + ' ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!')
+
+        }
+        else {
+          console.log('Hello' + ' ' + nama +', pilih peranmu untuk memulai game!');
+        }
+      }
+    }
+  }
+}
