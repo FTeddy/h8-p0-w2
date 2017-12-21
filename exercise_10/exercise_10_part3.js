668a245c01975edb4d890c5ddf79aaf34f32f03e
FTeddy <ainsophoure@gmail.com>
2017-12-21 14:58:58 +0700

    error fixes to e12,e10-2,e10-3

---
+++ exercise_10/exercise_10_part3.js
@@ -0,0 +1,10 @@
+//exercise part 3
+
+var bintang='';
+for(index1=1; index1<=10 ;index1++){
+  for(index2=0; index2<=index1 ; index2++){
+    bintang = bintang + '*';
+  }
+  console.log(bintang);
+  var bintang='';
+}
--- exercise_12.js
+++ exercise_12.js
@@ -2,7 +2,7 @@
 function balikString(inputWord){
   // console.log(input.length)
   var output=''
-  for(var index=input.length-1; index>=0; index--){
+  for(var index=inputWord.length-1; index>=0; index--){
     output = output + inputWord[index] //insert letters from input with reversed index no. as string
   }
   return output
@@ -18,7 +18,7 @@ function balikString(inputWord){
   // console.log(input.length)
   var output_array=[]
   var result
-  for(var index=input.length-1; index>=0; index--){
+  for(var index=inputWord.length-1; index>=0; index--){
     output_array.push(inputWord[index]) //insert letters from input as array in reverse index
   }
   result=output_array.join('') //join array as string
